d1135bdfb5ffa5d8ab82af6834bfc5a66de6ff4b
DavidCD <davidcdaniels@hotmail.com>
2023-03-14 23:37:27 -0400

    Added Small Random Offset To Plants
    -Read Above
    -Used Random of (-1,1) To simulate offset for X,Y of Plant Position

--- Plants/Plant.cs
+++ Plants/Plant.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using NoiseGenProject.Helpers;
+using Plantlife.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,19 +17,32 @@ namespace Plantlife.Plants
         public int CurrFrame = 0;
         public int MaxFrames;
         public Vector2 Origin;
+        public Vector2 Position;
         public bool isHarvestable = false;
+
+        private int xOffset;
+        private int yOffset;
 
         public Plant(SpriteAnimation TextureAnim)
         {
             this.TextureAnim = TextureAnim;
             TextureAnim.setFrame(0);
+            CreateRandom();
         }
 
         public void Draw(SpriteBatch _spriteBatch, float Depth, Vector2 Position)
         {
-            TextureAnim.Position = Position - new Vector2(0, TextureAnim.Texture.Height / 2f);
+            TextureAnim.Position = Position - new Vector2(xOffset, (TextureAnim.Texture.Height / 2f) + yOffset);
             Origin = new Vector2(Position.X - 16, Position.Y + 32);
             TextureAnim.Draw(_spriteBatch, Depth);
+        }
+
+
+        private void CreateRandom()
+        {
+            Random rand = new Random();
+            xOffset = rand.Next(-1, 1);
+            yOffset = rand.Next(-1, 1);
         }
 
         public void Update()
